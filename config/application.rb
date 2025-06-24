cc05fc7280452dd93e2ba6ba81251197aa150d65
山田幸助 <offmailbox1225@gmail.com>
2025-06-25 08:20:36 +0900

    tailwindのパス削除

--- config/application.rb
+++ config/application.rb
@@ -23,12 +23,5 @@ module RailsEcSite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    # tailwindcss-rails gem が使用する Tailwind CSS の入力ファイルを指定。
-    # デフォルトパスからの変更や、esbuild との連携のために明示的に設定。
-    config.tailwindcss.input = "app/assets/stylesheets/application.tailwind.css"
-
-    # Tailwind CSS のビルド成果物の出力先パスを指定。
-    # esbuild が出力するアセットディレクトリ (app/assets/builds) に合わせ、最終的な application.css に Tailwind スタイルが統合される。
-    config.tailwindcss.output = "app/assets/builds/application.css"
   end
 end
