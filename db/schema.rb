a4d03efd1bcb39be821784dab6f5e2f2bc857629
0507osaOT <o.tsuboi0507@gmail.com>
2025-07-01 23:57:45 +0900

    Fix user email validation and test fixtures

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_23_154501) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_01_143822) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
