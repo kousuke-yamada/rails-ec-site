48bb2931ad41753c186024b43919d38484a76983
0507osaOT <o.tsuboi0507@gmail.com>
2025-07-22 18:52:16 +0900

    ログアウト機能修正とログインページ中央配置対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,9 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(resource)
     home_top_path
   end
+
+  # Deviseのログアウト後のリダイレクト先を指定
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path # ログインページにリダイレクト
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' } # この行を確認
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations' 
+  }
+  
   get 'home/top', as: :home_top
   root 'home#top'
 end
