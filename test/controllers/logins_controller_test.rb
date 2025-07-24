bbb3d879c4af244abcf329103fe7a3bdf6c4b75a
0507osaOT <o.tsuboi0507@gmail.com>
2025-07-24 23:00:39 +0900

    テストコード変更

--- test/controllers/logins_controller_test.rb
+++ test/controllers/logins_controller_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class LoginsControllerTest < ActionDispatch::IntegrationTest
+    fixtures :users
   test "should get new" do
+    user = users(:one)
     get logins_new_url
     assert_response :success
   end
