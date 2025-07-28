506d610abd43f4a7dd15ac49aa6ac44cfb25dcee
山田幸助 <offmailbox1225@gmail.com>
2025-07-28 21:34:23 +0900

    rubocop対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  root to: "home#top"  
+  root to: "home#top"
 
   get "/mypage", to: "users#mypage"
   get "logins/new", to: "logins#new"
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
       get :listing
       get :my_products  # この行を追加
     end
-  end  
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -25,5 +25,4 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: "users/registrations"
   }
-  
 end
