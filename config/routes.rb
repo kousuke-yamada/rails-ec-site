8bdb81c92a71ca201f89c668d01045a717d9db30
OT <o.tsuboi0507@gmail.com>
2025-09-25 22:36:50 +0900

    必要ないloginを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # config/routes.rb
 Rails.application.routes.draw do
   root to: "home#top"
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
   get "/mypage", to: "users#mypage"
 
   resources :products do
